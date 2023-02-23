3df688abf045369768fdfd53623c2af691754ab4
Alina Baltsatu <baltsatu.a@gmail.com>
2023-02-23 22:38:02 +0200

    Modifying Sign_In_Page & adding UI tests

--- modules/ui/page_objects/sign_in_page.py
+++ modules/ui/page_objects/sign_in_page.py
@@ -9,15 +9,28 @@ class SignInPage(BasePage):
         super().__init__()
 
     def go_to(self):
-        self.driver.get(SignInPage.URL)
+        self.driver.get(self.URL)
 
-    def try_login(self, username, password):
-        login_elem = self.driver.find_element(By.ID, "login_field")
-        login_elem.send_keys(username)
-        pass_elem = self.driver.find_element(By.ID, "password")
-        pass_elem.send_keys(password)
-        btn_elem = self.driver.find_element(By.NAME, "commit")
-        btn_elem.click()
+    def enter_username(self, username):
+        login_field = self.driver.find_element(By.ID, "login_field")
+        login_field.send_keys(username)
+
+    def enter_password(self, password):
+        password_field = self.driver.find_element(By.ID, "password")
+        password_field.send_keys(password)
+
+    def click_sign_in(self):
+        sign_in_button = self.driver.find_element(By.NAME, "commit")
+        sign_in_button.click()
+
+    def click_forgot_password(self):
+        forgot_password_link = self.driver.find_element(By.XPATH, '//*[@id="login"]/div[4]/form/div/a')
+        forgot_password_link.click()
 
     def check_title(self, expected_title):
         return self.driver.title == expected_title
+    
+    def check_error_message(self, expected_error):
+        error_elem = self.driver.find_element(By.CLASS_NAME, "js-flash-alert")
+        return error_elem.text == expected_error
+    
--- tests/ui/test_ui_page_object.py
+++ tests/ui/test_ui_page_object.py
@@ -6,6 +6,27 @@ import pytest
 def test_check_incorrest_username_page_object():
     sign_in_page = SignInPage()
     sign_in_page.go_to()
-    sign_in_page.try_login("wrong_email@gmail.com", "wrong_password")
+    sign_in_page.enter_username("wrong_email@gmail.com")
+    sign_in_page.enter_password("wrong_password")
+    sign_in_page.click_sign_in()
     assert sign_in_page.check_title("Sign in to GitHub · GitHub")
     sign_in_page.close()
+
+
+@pytest.mark.ui
+def test_login_with_empty_fields():
+    sign_in_page = SignInPage()
+    sign_in_page.go_to()
+    sign_in_page.click_sign_in()
+    assert sign_in_page.check_error_message("Incorrect username or password.")
+    sign_in_page.close()      
+
+
+@pytest.mark.ui
+def test_check_opening_forgot_password_page():
+    sign_in_page = SignInPage()
+    sign_in_page.go_to()
+    sign_in_page.click_forgot_password()
+    assert sign_in_page.check_title("Forgot your password? · GitHub")
+    sign_in_page.close()  
+
